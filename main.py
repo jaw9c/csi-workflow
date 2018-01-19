be0cc0fb8eaa4ecd724081cb29451fefa0446797
Josh Warwick <josh.warwick15@gmail.com>
2018-01-19 09:50:50 +0000

    chnages

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from clint.textui import prompt
 from trello import TrelloClient
 from config import TRELLO_API_KEY, TRELLO_API_SECRET
 from clint.textui import colored, puts
-import config 
+import config
+import subprocess
+import os
+
 class doit:
 	board_id = config.BOARD_ID
 
@@ -30,7 +33,7 @@ class doit:
 		ticket_number = prompt.query('Ticket Number: ') 
 		puts(colored.yellow('Confirm ticket #' + str(ticket_number)))
 		if prompt.query('Confirm: y/n: ') == 'y':
-			self.ticket_number = ticket_number
+			self.ticket_number = int(ticket_number)
 			puts(colored.green('#' + str(ticket_number) + ' confirmed!'))
 		else: 
 			self.get_ticket_number()
@@ -38,10 +41,48 @@ class doit:
 	def get_trello_board(self):
 		print('hi')
 
+	def send_pr(self, pr_title):
+		os.system("git pr")
+ 
+	def get_card(self):
+		for card in self.trello_client.get_list(config.BACKLOG_LIST_ID).list_cards():
+			if card.short_id == self.ticket_number:
+				self.card = card
+				self.card_title = card.name.split(") ", 1)[1] 
+				return
+		puts(colored.red('Card not found in backlog with number: #' + str(self.ticket_number)))
+	def process(self): 
+		for i in range(len(config.PROCESS_LIST_IDS[1:])):
+			list = self.trello_client.get_list(config.PROCESS_LIST_IDS[i+1])
+			input = prompt.query('Move to: ' + list.name + '? y/n')
+			if input == 'y':
+				self.card.change_list(list.id)
+				self.card.set_pos('bottom')
+				puts(colored.green('Moved!'))
+				if list.id == config.PR_LIST_ID:
+					puts(colored.yellow('Submitting PR'))
+					self.pull_request()
+					puts(colored.green('Submitted!')) 
+	def pull_request(self):
+		os.system("git push")
+		master_command = "hub pull-request -m \"" + self.card_title + "\" -h `git rev-parse --abbrev-ref HEAD` -b master -l \"master\""
+		staging_command = "hub pull-request -m \"" + self.card_title + "\" -h `git rev-parse --abbrev-ref HEAD` -b staging -l \"staging\"" 
 
+		master = subprocess.check_output(master_command, shell=True)
+		staging = subprocess.check_output(staging_command, shell=True)
+
+		self.master_pr = str(master)[2:].split("\\")[0]
+		self.staging_pr = str(staging)[2:].split("\\")[0]
+		
+		puts(colored.green('Master link: ' + self.master_pr))
+		puts(colored.green('Staging link: ' + self.staging_pr))
+			
+	
 	def do(self):
 		self.intialise_trello()
-		self.get_board()
+		self.get_ticket_number()
+		self.get_card()
+		self.process()
 
 maindo = doit()
 maindo.do()
